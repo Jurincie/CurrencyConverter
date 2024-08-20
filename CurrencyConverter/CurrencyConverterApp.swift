b841c64e37805a93f5eac36de2e409b0581ef436
RonJurincie <ronjurincie@gmail.com>
2024-08-20 17:04:57 -0400

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Ron Jurincie on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
